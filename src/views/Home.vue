0e5ff6e41f2337f31ef1053841883fe373df6071
Carlos Becerra <carlosbecerra.dev@gmail.com>
2021-05-08 18:01:16 -0500

    :sparkles: Add info.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,17 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <h1>Flujo</h1>
+    <ol>
+      <li>Usuario INICIA SESIÓN (login)</li>
+      <li>Usuario REALIZA FORMULARIO y ENVÍA solicitud</li>
+      <li>Sistema ENVÍA mensaje al email de los usuario "Solicitud recibida".</li>
+      <li>Administrador en su Dashboard VIASUALIZA Solicitud de los usuario (Incidencia o Requerimiento).</li>
+      <li>Administrador ASIGNA técnico y GENERA ticket de atención</li>
+      <li>Técnico en su Dashboard VIASUALIZA los ticket de atención generados y asignados</li>
+      <li>Técnico ATIENDE ticket de atención</li>
+      <li>Sistema CAMBIA estado del ticket de atención y MUEVE al panel de "En proceso"</li>
+      <li>Técnico puede TERMINAR o DELEGAR ticket de atención</li>
+      <li>Administrador GESTIONA a técnicos</li>
+    </ol>
+  </div>
 </template>
